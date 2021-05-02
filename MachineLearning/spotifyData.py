4ef901d7bc6d4fa197d3a8879f966c505192962c
dansVibin <78011366+dansVibin@users.noreply.github.com>
2021-05-02 00:39:19 -0700

    Add files via upload

--- MachineLearning/spotifyData.py
+++ MachineLearning/spotifyData.py
@@ -8,7 +8,7 @@ from pprint import pprint
 SPOTIPY_CLIENT_ID= 'f792fe7782384f54879ed2ae80538895'
 SPOTIPY_CLIENT_SECRET = '11d8603456734c3db24cfc54eaa1bd63'
 
-playlistLink = 'https://open.spotify.com/playlist/7uzmW6AP48YD006g3YGVEo?si=0260ea551a2a4804'
+playlistLink = 'https://open.spotify.com/playlist/3SBQw96qznmFZ5KkqnmPyK?si=Z3bxmA1QT3eMt02xWdxIzA&dl_branch=1'
 
 apiLink = sp.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = SPOTIPY_CLIENT_ID,client_secret = SPOTIPY_CLIENT_SECRET))
 
@@ -33,10 +33,10 @@ for i in range(0,totalTracks):
 
     audioFeatures.append(apiLink.audio_features(trackInfo['items'][i]['track']['uri']))
 
-
+audioFeatures.insert(0,audioFeatures[0])
 trackMetaDict = {'name':trackName,'artist':trackArtist, 'genre': genreTags}
 trackMetaDF = pd.DataFrame(trackMetaDict)
 audioFeaturesDF = pd.DataFrame(audioFeatures)
 
-trackMetaDF.to_csv(r'C:\Users\enigm\Documents\School\FourthYear\Spring21\ECE196\machineLearning\trackInfo2.csv')
-audioFeaturesDF.to_csv(r'C:\Users\enigm\Documents\School\FourthYear\Spring21\ECE196\machineLearning\audioFeatures2.csv')
+trackMetaDF.to_csv(r'C:\Users\dlope\Documents\school\spring21\ece196\trackInfoNICK.csv')
+audioFeaturesDF.to_csv(r'C:\Users\dlope\Documents\school\spring21\ece196\audioFeaturesNICK.csv')
