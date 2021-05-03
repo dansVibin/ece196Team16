6b9bd8bb94548653b9a2f780188dc81577547b17
dansVibin <78011366+dansVibin@users.noreply.github.com>
2021-05-03 00:01:27 -0700

    Add files via upload

--- MachineLearning/machineLearning.m
+++ MachineLearning/machineLearning.m
@@ -1,54 +1,87 @@
+%% ECE 196 Predicting Musical Genres Using Machine Learning
+% Daniel Lopez Villa -- Jimmy Do
+% A14870615 -- A15110598
+% An Exploratory Study of the k-Nearest Neighbors Algorithm
+%% Importing Model Data
 clear; close all; clc;
-%% Importing MODEL Data
 audioFeatures = readtable('audioFeatures.csv');
 trackInfo = readtable('trackInfo.csv');
 
-trackInfo(:,1) = [];
-genreTags = table2cell(trackInfo(:,3));
+trackInfo(:,1) = [];%delete track number column 
+genreTags = table2cell(trackInfo(:,3)); %extract genreTag data
 genreList = ["Classical" , "80s" , "Hip Hop" , "Metal"];
 
-songs = [1:height(audioFeatures)];
-numSongs = length(songs);
-numGenres = 4;
-songsPerGenre = numSongs/numGenres;
+numSongs = height(audioFeatures);
+songs = 1:numSongs;%vector of 1:song numbers
+numGenres = 4; 
 
-Genre = string(80*ones(numSongs,1));
+%genre list default to 80s
+Genre = string(80*ones(numSongs,1))+"s";
 
 songMean = zeros(numGenres,1);
 average = zeros(numGenres,1);
 
 %organizing labels/data
-audioFeaturesLabels = string(table2cell(audioFeatures(1,1:2:width(audioFeatures))));
+pattern = lettersPattern; 
+audioFeaturesLabels = extract(erase(string(table2cell(audioFeatures(1,1:2:width(audioFeatures)))),"_"),pattern);
+
+%remove unwanted features
 audioFeaturesLabels(:,end) = [];
 audioFeaturesLabels(:,12:16) = [];
+
+%remove unwanted features
 audioFeaturesData = audioFeatures(:,2:2:width(audioFeatures));
 audioFeaturesData(:,end) = [];
 audioFeaturesData(:,12:16) = [];
-audioFeaturesDataNew = zeros(height(audioFeatures),width(audioFeaturesData));
 
+audioFeaturesDataNew = zeros(height(audioFeatures),width(audioFeaturesData));
 for i = 1:height(audioFeatures)
-    
-    for j = 1 : width(audioFeaturesData)
-        audioFeaturesDataNew(i,j) = str2num(cell2mat(table2array(audioFeaturesData(i,j))));
-        %if (i ==1) audioFeaturesLabels{j}(1) = upper(audioFeaturesLabels{j}(1)); 
-        %end
-    end
     for k=1:numGenres
         if (contains(genreTags{i},lower(genreList(k)))) == 1
             Genre(i,1) = genreList(k); end
     end
-    
+    for j = 1 : width(audioFeaturesData)
+        audioFeaturesDataNew(i,j) = str2num(cell2mat(table2array(audioFeaturesData(i,j))));
+        if (i==1) audioFeaturesLabels{j}(1) = upper(audioFeaturesLabels{j}(1)); 
+        end
+    end
 end
+
+songsPerGenre = zeros(numGenres+1,1);
+for i = 1:numGenres
+    for j = 1:width(audioFeaturesDataNew)
+        audioFeat4{i}(:,j) = audioFeaturesDataNew(find(Genre == genreList(i)),j);
+    end 
+    songsPerGenre(i+1) = height(audioFeat4{i}); 
+end
+
+%present final data set
+Dancibility = audioFeaturesDataNew(:,1);
+Energy = audioFeaturesDataNew(:,2);
+Key = audioFeaturesDataNew(:,3);
+Loudness = audioFeaturesDataNew(:,4);
+mode = audioFeaturesDataNew(:,5);
+Speechiness = audioFeaturesDataNew(:,6);
+Acousticness = audioFeaturesDataNew(:,7);
+Instrumentalness = audioFeaturesDataNew(:,8);
+Liveness = audioFeaturesDataNew(:,9);
+Valence = audioFeaturesDataNew(:,10);
+Tempo = audioFeaturesDataNew(:,11);
+Duration = audioFeaturesDataNew(:,12);
+
+musicDataSet = table(Dancibility,Energy,Valence,Acousticness,Instrumentalness,Genre);
+LabelsUsed = ["Dancibility","Energy","Valence","Acousticness","Instrumentalness"];
+musicDataSet2 = table(Dancibility,Energy,Valence,Acousticness,Speechiness,Instrumentalness,Genre);
 %% Plotting
+audioFeatBox = cell(1,width(audioFeaturesData));
 for i = 1:width(audioFeaturesData)
     figure(i)
-        for j = 1:numGenres
-            genreSection = [(songsPerGenre*j-songsPerGenre+1):songsPerGenre*j];
+        for j = 1:numGenres          
+            genreSection = sum(songsPerGenre(1:j))+1 : sum(songsPerGenre(1:j+1));
             songMean(j) = mean(genreSection);
-            average(j) = mean(audioFeaturesDataNew(genreSection,i));
-            scatter(songs(genreSection),audioFeaturesDataNew(genreSection,i),50,'filled')
+            average(j) = mean(audioFeat4{j}(:,i));
+            scatter(genreSection,audioFeat4{j}(:,i),50,'filled')
             hold on 
-            audioFeat4{i}(:,j) = audioFeaturesDataNew(genreSection,i);
         end
     plot(songMean,average,'--k.','Linewidth',2','Markersize',34)
     hold on
@@ -61,25 +94,9 @@ for i = 1:width(audioFeaturesData)
     legend(["Classical" , "80s" , "Hip Hop" , "Metal" , "Average"],'Location',location)
 end
 
-Dancibility = reshape(audioFeat4{1},[numSongs,1]);
-Energy = reshape(audioFeat4{2},[numSongs,1]);
-Key = reshape(audioFeat4{3},[numSongs,1]);
-Loudness = reshape(audioFeat4{4},[numSongs,1]);
-mode = reshape(audioFeat4{5},[numSongs,1]);
-Speechiness = reshape(audioFeat4{6},[numSongs,1]);
-Acousticness = reshape(audioFeat4{7},[numSongs,1]);
-Instrumentalness = reshape(audioFeat4{8},[numSongs,1]);
-Liveness = reshape(audioFeat4{9},[numSongs,1]);
-Valence = reshape(audioFeat4{10},[numSongs,1]);
-Tempo = reshape(audioFeat4{11},[numSongs,1]);
-Duration = reshape(audioFeat4{12},[numSongs,1]);
-
-musicDataSet = table(Dancibility,Energy,Valence,Acousticness,Instrumentalness,Genre);
-musicDataSet2 = table(Dancibility,Energy,Valence,Acousticness,Speechiness,Instrumentalness,Genre);
-
 for i = 1:width(audioFeaturesData)
     figure(width(audioFeaturesData)+i)
-    boxplot(audioFeat4{i},genreList)
+    boxplot(audioFeatBox{i},genreList)
     title('Audio Feature by Genre')
     hold on
     grid on
@@ -93,12 +110,11 @@ trackInfoTEST(:,1) = [];
 
 songsTEST = [1:height(audioFeaturesTEST)];
 numSongsTEST = length(songsTEST);
-songsPerGenreTEST = numSongsTEST/numGenres;
 songMeanTEST = zeros(numGenres,1);
 averageTEST = zeros(numGenres,1);
 genreTagsTEST = table2cell(trackInfoTEST(:,3));
 
-GenreTEST = string(80*ones(numSongsTEST,1));
+GenreTEST = string(80*ones(numSongsTEST,1))+"s";
 
 audioFeaturesDataTEST = audioFeaturesTEST(:,2:2:width(audioFeaturesTEST));
 audioFeaturesDataTEST(:,end) = [];
@@ -106,26 +122,47 @@ audioFeaturesDataTEST(:,12:16) = [];
 
 audioFeaturesDataTESTNew = zeros(height(audioFeaturesTEST),width(audioFeaturesDataTEST));
 for i = 1:height(audioFeaturesTEST)
-    for j = 1 : width(audioFeaturesDataTEST)
-        audioFeaturesDataTESTNew(i,j) = str2num(cell2mat(table2array(audioFeaturesDataTEST(i,j))));
-        if (i ==1) audioFeaturesLabels{j}(1) = upper(audioFeaturesLabels{j}(1)); 
-        end
-    end
     for k=1:numGenres
         if (contains(genreTagsTEST{i},lower(genreList(k)))) == 1
             GenreTEST(i,1) = genreList(k); end
     end
+    for j = 1 : width(audioFeaturesDataTEST)
+        audioFeaturesDataTESTNew(i,j) = str2num(cell2mat(table2array(audioFeaturesDataTEST(i,j))));
+    end
 end
+
+songsPerGenreTEST = zeros(numGenres+1,1);
+for i = 1:numGenres
+    for j = 1:width(audioFeaturesDataTESTNew)
+        audioFeat4TEST{i}(:,j) = audioFeaturesDataTESTNew(find(GenreTEST == genreList(i)),j);
+    end 
+    songsPerGenreTEST(i+1) = height(audioFeat4TEST{i}); 
+end
+
+DancibilityTEST = audioFeaturesDataTESTNew(:,1);
+EnergyTEST = audioFeaturesDataTESTNew(:,2);
+KeyTEST = audioFeaturesDataTESTNew(:,3);
+LoudnessTEST = audioFeaturesDataTESTNew(:,4);
+modeTEST = audioFeaturesDataTESTNew(:,5);
+SpeechinessTEST = audioFeaturesDataTESTNew(:,6);
+AcousticnessTEST = audioFeaturesDataTESTNew(:,7);
+InstrumentalnessTEST = audioFeaturesDataTESTNew(:,8);
+LivenessTEST = audioFeaturesDataTESTNew(:,9);
+ValenceTEST = audioFeaturesDataTESTNew(:,10);
+TempoTEST = audioFeaturesDataTESTNew(:,11);
+DurationTEST = audioFeaturesDataTESTNew(:,12);
+
+musicDataSetTEST = [DancibilityTEST EnergyTEST ValenceTEST AcousticnessTEST InstrumentalnessTEST];
+musicDataSetTEST2 = [DancibilityTEST EnergyTEST ValenceTEST AcousticnessTEST SpeechinessTEST InstrumentalnessTEST];
 %% Plotting
 for i = 1:width(audioFeaturesDataTEST)
-    figure(i+24)
+    figure(i+12)
         for j = 1:numGenres
-            genreSection = [(songsPerGenreTEST*j-songsPerGenreTEST+1):songsPerGenreTEST*j];
+            genreSection = sum(songsPerGenreTEST(1:j))+1 : sum(songsPerGenreTEST(1:j+1));
             songMeanTEST(j) = mean(genreSection);
-            averageTEST(j) = mean(audioFeaturesDataTESTNew(genreSection,i));
-            scatter(songsTEST(genreSection),audioFeaturesDataTESTNew(genreSection,i),50,'filled')
+            averageTEST(j) = mean(audioFeat4TEST{j}(:,i));
+            scatter(genreSection,audioFeat4TEST{j}(:,i),50,'filled')
             hold on 
-            audioFeat4TEST{i}(:,j) = audioFeaturesDataTESTNew(genreSection,i);
         end
     plot(songMeanTEST,averageTEST,'--k.','Linewidth',1','Markersize',17)
     hold on
@@ -137,40 +174,50 @@ for i = 1:width(audioFeaturesDataTEST)
     else location = 'Northeast';end
     legend(["Classical" , "80s" , "Hip Hop" , "Metal" , "Average"],'Location',location)
 end
-
-DancibilityTEST = reshape(audioFeat4TEST{1},[numSongsTEST,1]);
-EnergyTEST = reshape(audioFeat4TEST{2},[numSongsTEST,1]);
-KeyTEST = reshape(audioFeat4TEST{3},[numSongsTEST,1]);
-LoudnessTEST = reshape(audioFeat4TEST{4},[numSongsTEST,1]);
-modeTEST = reshape(audioFeat4TEST{5},[numSongsTEST,1]);
-SpeechinessTEST = reshape(audioFeat4TEST{6},[numSongsTEST,1]);
-AcousticnessTEST = reshape(audioFeat4TEST{7},[numSongsTEST,1]);
-InstrumentalnessTEST = reshape(audioFeat4TEST{8},[numSongsTEST,1]);
-LivenessTEST = reshape(audioFeat4TEST{9},[numSongsTEST,1]);
-ValenceTEST = reshape(audioFeat4TEST{10},[numSongsTEST,1]);
-TempoTEST = reshape(audioFeat4TEST{11},[numSongsTEST,1]);
-DurationTEST = reshape(audioFeat4TEST{12},[numSongsTEST,1]);
-
-musicDataSetTEST = [DancibilityTEST EnergyTEST ValenceTEST AcousticnessTEST InstrumentalnessTEST];
-musicDataSetTEST2 = [DancibilityTEST EnergyTEST ValenceTEST AcousticnessTEST SpeechinessTEST InstrumentalnessTEST];
 %% KNN Model
-tic
-model1 = fitcknn(musicDataSet(:,1:end-1),musicDataSet(:,end))
-nieghbors1 = 3;
-model1.NumNeighbors = nieghbors1;
-Predicted = string(predict(model1,musicDataSetTEST));
+maxNeighbors = 40;
+distIter = ['minkowski','chebychev'];
+PredictedTrain = cell(maxNeighbors,1);
+PredictedTest = cell(maxNeighbors,1);
+AccuracyTrain = zeros(maxNeighbors,1);
+AccuracyTest = zeros(maxNeighbors,1);
+for k = 1:maxNeighbors
+    model = fitcknn(musicDataSet(:,1:end-1),musicDataSet(:,end));
+    model.NumNeighbors = k;
+    model.Distance = 'chebychev';
+    PredictedTrain{k} = string(predict(model,musicDataSet));
+    PredictedTest{k} = string(predict(model,musicDataSetTEST));
+    
+    Error(k) = resubLoss(model);
+    AccuracyTrain(k) = 100*(sum(PredictedTrain{k} == Genre)/length(PredictedTrain{k}));
+    AccuracyTest(k) = 100*(sum(PredictedTest{k} == GenreTEST)/length(PredictedTest{k}));
+end
 
-toc
-model2 = fitcknn(musicDataSet2(:,1:end-1),musicDataSet2(:,end))
-nieghbors2 = 3;
-model2.NumNeighbors = nieghbors2;
-Predicted2 = string(predict(model2,musicDataSetTEST2));
+figure(25)
+plot(1:maxNeighbors,Error,'--k.','Linewidth',1','Markersize',21)
+hold on
+grid on
+xlabel('K Neighbors')
+ylabel('Error')
+title('Error of Model Across Various K Values [Chebychev]')
 
-Accuracy = 100 * (sum(Predicted == GenreTEST)/length(Predicted));
-Accuracy2 = 100 * (sum(Predicted2 == GenreTEST)/length(Predicted));
+figure(26)
+plot(1:maxNeighbors,AccuracyTrain,'--k.','Linewidth',1','Markersize',21)
+hold on
+grid on
+xlabel('K Neighbors')
+ylabel('Accuracy [%]')
+title('Accuracy of Model Training Across Various K Values [Chebychev]')
 
-Results = table(Predicted,GenreTEST) 
-disp("Model 1 Accuracy ("+ string(nieghbors2) + " neighbors) = " + string(Accuracy) + "%")
-disp("Model 2 Accuracy ("+ string(nieghbors2) + " neighbors) = " + string(Accuracy2) + "%")
+figure(27)
+plot(1:maxNeighbors,AccuracyTest,'--k.','Linewidth',1','Markersize',21)
+hold on
+grid on
+xlabel('K Neighbors')
+ylabel('Accuracy [%]')
+title('Accuracy of Model Prediction Across Various K Values [Chebychev]')
+
+disp("Model 1 Accuracy ("+ string(3) + " neighbors) = " + string(AccuracyTest(3)) + "%")
+disp("Model 2 Accuracy ("+ string(4) + " neighbors) = " + string(AccuracyTest(4)) + "%")
 
 
